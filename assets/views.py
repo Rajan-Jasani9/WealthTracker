15c2a308b9dcda99b9fa7602d87f9df0aa3b9dfa
Rajan Jasani <79624392+Rajan-Jasani9@users.noreply.github.com>
2024-11-27 17:45:17 +0000

    feat: Debts and lenders API

--- assets/views.py
+++ assets/views.py
@@ -9,96 +9,97 @@ from .models import Asset
 # Create your views here.
 class CreateListAssetsView(GenericAPIView):
 
-  def post(self, request):
+    def post(self, request):
 
-    input_serializer = serializers.AssetInputSerializer(data=request.data)
-    if input_serializer.is_valid():
-      input_serializer.save()
-      return Response(data={
-          "status": status.HTTP_201_CREATED,
-          "message": "Asset created successfully",
-          "data": input_serializer.data,
-      },
-                      status=status.HTTP_201_CREATED)
+        input_serializer = serializers.AssetInputSerializer(data=request.data)
+        if input_serializer.is_valid():
+            input_serializer.save()
+            return Response(data={
+                "status": status.HTTP_201_CREATED,
+                "message": "Asset created successfully",
+                "data": input_serializer.data,
+            },
+                            status=status.HTTP_201_CREATED)
 
-    else:
-      return Response(data={
-          "status": status.HTTP_400_BAD_REQUEST,
-          "message": "Invalid input",
-      },
-                      status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(data={
+                "status": status.HTTP_400_BAD_REQUEST,
+                "message": "Invalid input",
+            },
+                            status=status.HTTP_400_BAD_REQUEST)
 
-  def get(self, request):
+    def get(self, request):
 
-    serializer = serializers.AssetListSerializer(Asset.objects.all(),
-                                                 many=True)
+        serializer = serializers.AssetListSerializer(Asset.objects.all(),
+                                                     many=True)
 
-    return Response(data={
-        'status': status.HTTP_200_OK,
-        'message': 'Assets retrieved successfully',
-        'data': serializer.data,
-    },
-                    status=status.HTTP_200_OK)
-
-
-class UpdateDetailDeleteAssetsView(GenericAPIView):
-
-  def patch(self, request, id):
-
-    asset = Asset.objects.get(id=id)
-    if not asset:
-      return Response(data={
-          "status": status.HTTP_404_NOT_FOUND,
-          "message": "Asset not found",
-      },
-                      status=status.HTTP_404_NOT_FOUND)
-
-    input_serializer = serializers.AssetInputSerializer(asset,
-                                                        data=request.data)
-    if input_serializer.is_valid():
-      input_serializer.save()
-      return Response(data={
-          "status": status.HTTP_200_OK,
-          "message": "Asset updated successfully",
-          "data": input_serializer.data,
-      },
-                      status=status.HTTP_200_OK)
-    else:
         return Response(data={
-            "status": status.HTTP_400_BAD_REQUEST,
-            "message": "Invalid input",
-        }, status=status.HTTP_400_BAD_REQUEST)
+            'status': status.HTTP_200_OK,
+            'message': 'Assets retrieved successfully',
+            'data': serializer.data,
+        },
+                        status=status.HTTP_200_OK)
 
-  def get(self, request, id):
-    asset = Asset.objects.get(id=id)
-    if not asset:
-      return Response(data={
-          "status": status.HTTP_404_NOT_FOUND,
-          "message": "Asset not found",
-      },
-                      status=status.HTTP_404_NOT_FOUND)
 
-    serializer = serializers.AssetDetailSerializer(asset)
+class DetailUpdateDeleteAssetsView(GenericAPIView):
 
-    return Response(data={
-        'status': status.HTTP_200_OK,
-        'message': 'Asset retrieved successfully',
-        'data': serializer.data,
-    },
-                    status=status.HTTP_200_OK)
+    def patch(self, request, id):
 
-  def delete(self, request, id):
-    asset = Asset.objects.get(id=id)
-    if not asset:
-      return Response(data={
-          "status": status.HTTP_404_NOT_FOUND,
-          "message": "Asset not found",
-      },
-                      status=status.HTTP_404_NOT_FOUND)
+        asset = Asset.objects.get(id=id)
+        if not asset:
+            return Response(data={
+                "status": status.HTTP_404_NOT_FOUND,
+                "message": "Asset not found",
+            },
+                            status=status.HTTP_404_NOT_FOUND)
 
-    asset.delete()
-    return Response(data={
-        "status": status.HTTP_200_OK,
-        "message": "Asset deleted successfully",
-    },
-                    status=status.HTTP_200_OK)
+        input_serializer = serializers.AssetInputSerializer(asset,
+                                                            data=request.data)
+        if input_serializer.is_valid():
+            input_serializer.save()
+            return Response(data={
+                "status": status.HTTP_200_OK,
+                "message": "Asset updated successfully",
+                "data": input_serializer.data,
+            },
+                            status=status.HTTP_200_OK)
+        else:
+            return Response(data={
+                "status": status.HTTP_400_BAD_REQUEST,
+                "message": "Invalid input",
+            },
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, id):
+        asset = Asset.objects.get(id=id)
+        if not asset:
+            return Response(data={
+                "status": status.HTTP_404_NOT_FOUND,
+                "message": "Asset not found",
+            },
+                            status=status.HTTP_404_NOT_FOUND)
+
+        serializer = serializers.AssetDetailSerializer(asset)
+
+        return Response(data={
+            'status': status.HTTP_200_OK,
+            'message': 'Asset retrieved successfully',
+            'data': serializer.data,
+        },
+                        status=status.HTTP_200_OK)
+
+    def delete(self, request, id):
+        asset = Asset.objects.get(id=id)
+        if not asset:
+            return Response(data={
+                "status": status.HTTP_404_NOT_FOUND,
+                "message": "Asset not found",
+            },
+                            status=status.HTTP_404_NOT_FOUND)
+
+        asset.delete()
+        return Response(data={
+            "status": status.HTTP_200_OK,
+            "message": "Asset deleted successfully",
+        },
+                        status=status.HTTP_200_OK)
